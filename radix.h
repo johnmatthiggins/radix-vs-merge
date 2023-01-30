a5f32fd35e9e1ee727925a608772262a6d0b8be0
John Higgins <johnmatthiggins@gmail.com>
2023-01-30 09:28:09 -0800

    Fixed compiler error

--- radix.h
+++ radix.h
@@ -5,8 +5,6 @@
 #include <queue>
 #include <cstdlib>
 
-void radix_sort(uint8_t* array, size_t size);
-void free_buckets(std::queue<uint8_t>** buckets);
-std::queue<uint8_t>** make_buckets();
+void radix_sort_8bit(uint8_t* array, size_t size);
 
 #endif
