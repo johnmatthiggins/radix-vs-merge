f83add7fa180d04704dd5a544c5aa94f73f473a1
John Higgins <johnmatthiggins@gmail.com>
2023-02-22 08:40:19 -0800

    Removed useless bit on README

--- README.md
+++ README.md
@@ -6,12 +6,3 @@ In this experiment, I will be examining the real world scaling of
 radix sort and merge sort. Both algorithms will be implemented in C++
 and used on both sets of 8-bit integers and 32-bit integers. These sets of integers
 will range from sizes of 2^1 to 2^30.
-
-## Implementation Details
-
-### Optimizations
-
-Both radix sort and merge sort have been optimized a little bit. First, in
-order to eliminate extra calls to dynamic memory allocation, I have given
-merge sort a single buffer in which it will store elements before performing
-a merge.
--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 import sys
 
 def main():
@@ -9,15 +10,37 @@ def main():
     radix8 = pd.read_csv('csv/radix.csv')
     merge32 = pd.read_csv('csv/merge32.csv')
     radix32 = pd.read_csv('csv/radix32.csv')
+    log_array = get_log_array(radix8.shape[0])
+    linear_array = get_linear_array(radix8.shape[0])
 
     plt.plot(merge32['N'], merge32['TIME'], label='merge (32-bit)')
     plt.plot(radix32['N'], radix32['TIME'], label='radix (32-bit)')
     plt.plot(merge8['N'], merge8['TIME'], label='merge (8-bit)')
     plt.plot(radix8['N'], radix8['TIME'], label='radix (8-bit)')
-    plt.yscale('log')
-    plt.xscale('log')
+    plt.plot(radix8['N'], log_array, label='N LOG N')
+    plt.plot(radix8['N'], linear_array, label='LINEAR')
+    # plt.yscale('log')
+    # plt.xscale('log')
+    plt.xlabel('Array Length (N)')
+    plt.ylabel('Runtime (T)')
     plt.legend()
     plt.show()
+
+
+def get_linear_array(length):
+    array = []
+    for i in range(0, length):
+        array.append(i)
+    
+    return array
+
+
+def get_log_array(length):
+    array = []
+    for i in range(1, length + 1):
+        array.append(i * math.log(i, 2))
+    
+    return array
 
 if __name__ == '__main__':
     main()
