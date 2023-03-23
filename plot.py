d4b09333a56094b18452030b95abce43fdcfa0be
John Higgins <johnmatthiggins@gmail.com>
2023-03-22 22:02:13 -0700

    Added results png

--- plot.py
+++ plot.py
@@ -17,14 +17,14 @@ def main():
     plt.plot(radix32['N'], radix32['TIME'], label='radix (32-bit)')
     plt.plot(merge8['N'], merge8['TIME'], label='merge (8-bit)')
     plt.plot(radix8['N'], radix8['TIME'], label='radix (8-bit)')
-    plt.plot(radix8['N'], log_array, label='N LOG N')
-    plt.plot(radix8['N'], linear_array, label='LINEAR')
+    # plt.plot(radix8['N'], log_array, label='N LOG N')
+    # plt.plot(radix8['N'], linear_array, label='LINEAR')
     # plt.yscale('log')
     # plt.xscale('log')
     plt.xlabel('Array Length (N)')
     plt.ylabel('Runtime (T)')
     plt.legend()
-    plt.show()
+    plt.savefig('results.png')
 
 
 def get_linear_array(length):
